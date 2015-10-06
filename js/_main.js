b530de39ca9969b8964a9f8b7fbb0899e6949e21
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-10-06 11:19:05 -0700

    Skinny Bones Blog Template

--- js/_main.js
+++ js/_main.js
@@ -2,10 +2,10 @@
 $(document).ready(function(){
   // Menu button click
   $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e){
-    // $('#js-body').toggleClass('no-scroll');
+    $('#js-body').toggleClass('no-scroll');
     $('#js-menu, #js-menu-screen').toggleClass('is-visible');
     $('#js-menu-trigger').toggleClass('slide close');
-    // $('#masthead, #page-wrapper').toggleClass('slide');
+    $('#masthead, #page-wrapper').toggleClass('slide');
     e.preventDefault();
   });
 });
